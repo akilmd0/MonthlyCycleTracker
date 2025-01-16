41d2d5eb1805d371bb51089adfc496fcdcb5cd35
akilmd0 <akilmd0@gmail.com>
2025-01-16 19:31:40 +0530

    Initial commit

---
+++ src/main/java/com/example/MonthlyCycleTracker/MonthlyCycleTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.MonthlyCycleTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlyCycleTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyCycleTrackerApplication.class, args);
+	}
+
+}
